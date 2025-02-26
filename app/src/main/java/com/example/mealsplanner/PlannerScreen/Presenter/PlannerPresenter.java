064c0922e7abac0d747734d81f41937dbca8ab87
kemad97 <kerolos.emad992@gmail.com>
2025-02-26 21:10:01 +0200

    plannerfrag

---
+++ app/src/main/java/com/example/mealsplanner/PlannerScreen/Presenter/PlannerPresenter.java
@@ -0,0 +1,4 @@
+package com.example.mealsplanner.PlannerScreen.Presenter;
+
+public interface PlannerPresenter {
+}
---
+++ app/src/main/java/com/example/mealsplanner/PlannerScreen/View/PlannerView.java
@@ -0,0 +1,4 @@
+package com.example.mealsplanner.PlannerScreen.View;
+
+public interface PlannerView {
+}
